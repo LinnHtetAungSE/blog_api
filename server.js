789149cdacc787ccd6d7b38a7eb58dd0676d4007
LinnHtet29 <linnh3064@gmail.com>
2024-01-24 14:31:54 +0630

    [add] cors back\

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    // origin: process.env.CLIENT,
+    origin: process.env.CLIENT,
     credentials: true,
   })
 );
